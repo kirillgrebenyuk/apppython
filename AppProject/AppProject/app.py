8f6ecded5849369d946dd8247ee162712c4f2081
Kirill Grebenyuk <grebenyuk@orem.su>
2020-10-29 18:00:09 +0300

    update 0.0.4

--- AppProject/AppProject/app.py
+++ AppProject/AppProject/app.py
@@ -10,16 +10,17 @@ import base64
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+app.title = "Система ИАС УЭР"           # Название сайта
+
 cnn = pyodbc.connect('DRIVER={SQL Server};PORT=port;SERVER=192.168.0.85;PORT=1433;DATABASE=Piramida2000;UID=sa;PWD=pswd')
-date = '2020-10-26'
+date = '2020-10-28'
 query = "SELECT dev.NAME,dat.PARNUMBER,dat.ITEM,dat.VALUE0,dat.VALUE1,dat.DATA_DATE FROM DATA as dat LEFT JOIN DEVICES as dev ON dat.OBJECT = dev.CODE WHERE dat.PARNUMBER = 12 AND dat.DATA_DATE > '2020-10-28' AND (dat.OBJECT IN(4407,4408,4409,4410,4411,4412,4413)) AND dat.ITEM = 1 ORDER BY dat.DATA_DATE ASC"
 df=pd.read_sql(query, con=cnn, parse_dates=['DATA_DATE'])
 
 cursor = cnn.cursor()
 cursor.execute(query)
 
-data_uri = base64.b64encode(open('', 'rb').read()).decode('utf-8')
-print(data_uri)
+data_uri = base64.b64encode(open('assets/unnamed.png', 'rb').read()).decode('utf-8')
 
 for row in cursor:
 	ch = row[0]
@@ -34,7 +35,11 @@ app.layout = html.Div([
 		data=df.to_dict('record'),
 		columns=[{'id': c, 'name': c} for c in df.columns],
         page_size=10
-	)
+	),
+    html.A(html.Img(src='data:image/png;base64,{}'.format(data_uri)), href='#zavod1'),
+    html.A(html.Img(src='data:image/png;base64,{}'.format(data_uri)), href='#zavod2'),
+    html.A(html.Img(src='data:image/png;base64,{}'.format(data_uri)), href='#zavod3'),
+    html.A(html.Img(src='data:image/png;base64,{}'.format(data_uri)), href='#zavod4')
 ])
 
 cnn.close()
